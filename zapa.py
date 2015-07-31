a2a5f9c296437b1487343ff93f973e9218fde6a2
Radek Podgorny <radek@podgorny.cz>
2015-07-31 13:53:44 +0100

    more zapa works

--- zapa.py
+++ zapa.py
@@ -3,6 +3,7 @@
 import logging
 import pymongo
 import jinja2
+import datetime
 
 
 # TODO: globals are shit
@@ -26,28 +27,67 @@ def load_zapa(fn):
 def zapareport1():
 	zapa = load_zapa('zapa.txt')
 
+	#since = datetime.datetime(2014, 7, 30)
+	since = datetime.datetime.now() - datetime.timedelta(hours=24)
+	till = datetime.datetime.now()
+
 	x = []
 	for mj, loc in zapa.items():
 		logging.info('processing %s' % mj)
 
-		v_last = None
-		t_last = None
-		query = {'k': {'$regex': '.*/ping6/%s\.asterix\.cz/ok' % mj}}
+		# just select the last one before since
+		query = {'k': {'$regex': '.*/ping6/%s\.asterix\.cz/ok' % mj}, 't': {'$lt': since}}
+		try:
+			doc = db.data.find(query).sort([('t', -1), ])[0]
+		except:
+			doc = None
+		#endtry
+
+		if doc:
+			state = doc['v']
+			state_since = doc['t']
+			t_last = doc['t']
+		else:
+			state = None
+			state_since = since
+			t_last = since
+		#endif
+
+		query = {'k': {'$regex': '.*/ping6/%s\.asterix\.cz/ok' % mj}, 't': {'$gte': since, '$lt': till}}
 		for doc in db.data.find(query).sort([('t', 1), ]):
 			k = doc['k']
 			v = doc['v']
 			t = doc['t']
 
-			if v != v_last:
-				x.append((mj, loc, 'vyp/zap', v, t))
-			elif not t_last or (t - t_last).total_seconds() > 3600:
-				x.append((mj, loc, 'mezera', v, t))
+			if v != state:
+				if state is None:
+					logging.debug('unk %s - %s' % (state_since, t))
+					x.append((mj, loc, 'unk', v, state_since, t))
+				elif state == 0:
+					logging.debug('vyp %s - %s' % (state_since, t))
+					x.append((mj, loc, 'vyp', v, state_since, t))
+				#endif
+
+				state = v
+				state_since = t
+			elif (t - t_last).total_seconds() > 3600:  # TODO: hard-coded shit
+				if state is None:
+					logging.debug('unk %s - %s' % (state_since, t))
+					x.append((mj, loc, 'unk', v, state_since, t))
+				elif state == 0:
+					logging.debug('vyp %s - %s' % (state_since, t))
+					x.append((mj, loc, 'vyp', v, state_since, t))
+				#endif
+
+				state = None
+				state_since = t
 			#endif
 
-			v_last = v
 			t_last = t
 		#endfor
 	#endfor
+
+	# TODO: the last record
 
 	env = jinja2.Environment(loader=jinja2.PackageLoader('zapa', 'templates'))
 	t = env.get_template('zapareport1.html')
